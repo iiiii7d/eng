b35ccb61f38c089069ad2846ecc5e9cbc15c41aa
i____7d <i.second.7d@gmail.com>
2020-10-02 12:51:01 +0000

    docs

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. eng documentation master file, created by
+   sphinx-quickstart on Fri Oct  2 12:47:57 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to eng's documentation!
+===============================
+test1
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+test2
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- main.py
+++ main.py
@@ -4,10 +4,15 @@ VERSION = "v0.1 (2/10/20)"
 import argparse
 import _engcompiler
 import traceback
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', metavar='f', type=str, nargs='?', help='file to run')
 args = parser.parse_args()
+
+if args.file == None:
+    print("Please run this script in the command prompt instead of the python shell. Example: 'python main.py example.eng'")
+    sys.exit()
 
 try:
     if not args.file.endswith(".eng"):
