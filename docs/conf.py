ee257b58b3dea515d33470ede9139bd8632b8ecb
i____7d <i.second.7d@gmail.com>
2020-10-04 06:29:28 +0000

    changed theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'master'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +55,4 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+highlight_language = 'none'
